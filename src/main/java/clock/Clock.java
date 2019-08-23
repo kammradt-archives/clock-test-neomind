34d687237bd4ca5fdf7c32391ba0ba10f8db4afe
kammradt <vinicius.kammradt1@gmail.com>
2019-08-22 21:59:44 -0300

    :sparkles: Adding the function to get an angle by a given time
    Now er are able to convert a given time with hours and minutes to the smallest angle between both pointers. My proccess of thinking will be added in the README.md file.

--- src/main/java/clock/Clock.java
+++ src/main/java/clock/Clock.java
@@ -4,8 +4,56 @@ import java.util.GregorianCalendar;
 
 public class Clock {
 
+    public double getAngleByTime(GregorianCalendar time) {
+        int minutes = time.getTime().getMinutes();
+        double additionalHours = minutesToHours(minutes);
 
-    public double getAngleByTime(GregorianCalendar calendar) {
-        return 0;
+        double hours = formatHours(time.getTime().getHours() + additionalHours);
+
+        double angleByHours = getAngleByHours(hours);
+        double angleByMintues = getAngleByMinutes(minutes);
+
+        double angleBetweenPointers = getAngleBetweenPointers(angleByHours, angleByMintues);
+
+        return getSmallestInternalAngle(angleBetweenPointers);
+
+    }
+
+    private double getSmallestInternalAngle(double angle) {
+        double smallestAngle;
+        if (angle > 180)
+            smallestAngle = 360 - angle;
+        else
+            smallestAngle = angle;
+        return smallestAngle;
+    }
+
+    private double getAngleBetweenPointers(double angleHours, double angleMinutes) {
+        double angleBetween;
+        if (angleHours >= angleMinutes)
+            angleBetween = angleHours - angleMinutes;
+        else
+            angleBetween = angleMinutes - angleHours;
+        return angleBetween;
+    }
+
+    private double getAngleByMinutes(int minutes) {
+        int degreesByMinute = 360/60;
+        return minutes * degreesByMinute;
+    }
+
+    private double getAngleByHours(double hours) {
+        int degreesByHour = 360/12;
+        return hours * degreesByHour;
+    }
+
+    private double formatHours(double hours) {
+        if (hours >= 12)
+            hours = hours - 12;
+        return hours;
+    }
+
+    private double minutesToHours(int minutes) {
+        return (double) minutes/60 ;
     }
 }
